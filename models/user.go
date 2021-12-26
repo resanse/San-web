3d4fadb04acd226506f03a3503c1e423c052cb0b
Sanser <92086648+resanse@users.noreply.github.com>
2021-12-26 18:10:45 +0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,26 +1,64 @@
-package cmd
+package models
 
 import (
-	"GinSql/controller"
+	"GinSql/dao"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	_"github.com/go-sql-driver/mysql"
+	"time"
 )
 
-func Entrance() {
-	router :=gin.Default()
-	router.POST("/register",controller.Register)
-	router.GET("/login",controller.Login)
-	router.POST("/charge",Logger(),controller.Charge)
-	router.Run()
+type user struct {
+	username string
+	password string
+	Balance int
 }
-func Logger() gin.HandlerFunc {
-	return func(context *gin.Context) {
 
-		cookie,err:=context.Request.Cookie("user_cookie")
-		if err != nil {
-			context.Abort()
+func Register(Username string,Password string) bool{
+	stm ,err:=dao.DB.Prepare("insert into table_test values (?,?,?) ")
+	if err != nil {
+		fmt.Println("insert failed ",err)
+		return false
+	}
+	defer  stm.Close()
+	_,err=stm.Exec(Username,Password,)
+	if err != nil {
+		fmt.Println("insert failed ",err)
+		return false
+	}
+	return true
+}
+func Login(Username string,Password string)  bool{
+	stm ,err :=dao.DB.Query("select * from table_test where username = ?",Username)
+	if err != nil {
+		fmt.Println("err ",err)
+		return false
+	}
+	defer stm.Close()
+	var u user
+	for stm.Next(){
+	err = stm.Scan(&u.username,&u.password)
+	if err !=nil{
+		fmt.Println("select err",err)
 		}
-		fmt.Println(cookie)
+	}
+	if u.password==Password {
+		return true
 
 	}
+	return false
+}
+func Charge(username string,money int,remark string) bool {
+	stm,err:=dao.DB.Prepare("update table_test set balance=? where username=?;")
+	if err !=nil {
+		return false
+	}
+	_,err=stm.Exec(money,username)
+	if err !=nil {
+		return false
+	}
+	_,err=dao.DB.Exec("insert into table_logs values (?,?,?,?) ",username,money,time.Now(),remark)
+	if err != nil {
+		return false
+	}
+	return true
 }
